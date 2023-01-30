30d3eb521bbfb457e2d7ab93610e6dfc20584738
max <max>
2023-01-30 11:35:57 -0600

    ready to start formatting layout

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,23 @@
 
   let currentWeather = {};
   let dailyWeather = {};
+  let time = []
+  let weathercode = []
+  let maxTemp = []
+  let minTemp = []
   let feelsLikeMax = [];
+  let feelsLikeMin = []
+  let sunriseTime = []
+  let sunsetTime = []
+  let totalPrecip = []
   let loaded = false;
-  let time
+  const dateFormat = new Intl.DateTimeFormat('en-us', {
+    weekday: "long",
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric"
+  })
+  const date = new Date()
 
   onMount(() => {
     navigator.geolocation.getCurrentPosition(positionSuccess, positionError);
@@ -16,10 +30,16 @@
         .then((response) => response.json())
         .then((data) => {
           currentWeather = data.current_weather;
-          dailyWeather = data.daily;
-          console.log(typeof dailyWeather)
-          feelsLikeMax = [...dailyWeather.apparent_temperature_max];
+          dailyWeather = data.daily
           time = [...dailyWeather.time]
+          weathercode = [...dailyWeather.weathercode]
+          maxTemp = [...dailyWeather.temperature_2m_max]
+          minTemp = [...dailyWeather.temperature_2m_min]
+          feelsLikeMax = [...dailyWeather.apparent_temperature_max];
+          feelsLikeMin = [...dailyWeather.apparent_temperature_min]
+          sunriseTime = [...dailyWeather.sunrise]
+          sunsetTime = [...dailyWeather.sunset]
+          totalPrecip = [...dailyWeather.precipitation_sum]
           loaded = true
         });
     }
@@ -38,6 +58,23 @@
     <p>Date: {new Date().toDateString()}</p>
   </div>
   <hr />
+  <div id="daily">
+    {#each time as day, idx }
+    <div class="day">
+      <h4>{dateFormat.format(date.setDate(date.getDate() + idx))}</h4>
+      <p>Weathercode: {weathercode[idx]}</p>
+      <p>High: {maxTemp[idx]}</p>
+      <p>Low: {minTemp[idx]}</p>
+      <p>FeelsHigh{feelsLikeMax[idx]}</p>
+      <p>FeelsLow{feelsLikeMin[idx]}</p>
+      <p>Rise:{sunriseTime[idx]}</p>
+      <p>Set: {sunsetTime[idx]}</p>
+      <p>Precip: {totalPrecip[idx]}</p>
+      <hr>
+    </div>
+      
+    {/each}
+  </div>
   {/if}
 </main>
 
