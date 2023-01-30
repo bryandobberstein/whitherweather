faf5988ea307d01d33e84bb896f678f6460e5fd2
max <max>
2023-01-30 12:31:32 -0600

    roughing out icons

--- src/App.svelte
+++ src/App.svelte
@@ -13,6 +13,34 @@
   let sunsetTime = []
   let totalPrecip = []
   let loaded = false;
+  const icons = {
+    0: "/icons/sunny.svg",
+    2: "/icons/cloud-sun.svg",
+    3: "/icons/cloud.svg",
+    45: "/icons/smog.svg",
+    48: "/icons/smog.svg",
+    51: "/icons/cloud-rain-solid.svg",
+    53: "/icons/cloud-rain-solid.svg",
+    55: "/icons/cloud-rain-solid.svg",
+    56: "/icons/cloud-rain-solid.svg",
+    57: "/icons/cloud-rain-solid.svg",
+    61: "/icons/cloud-rain-solid.svg",
+    63: "/icons/cloud-rain-solid.svg",
+    65: "/icons/cloud-showers-heavy.svg",
+    66: "/icons/cloud-rain-solid.svg",
+    67: "/icons/cloud-showers-heavy.svg",
+    71: "/icons/snowflake.svg",
+    73: "/icons/snowflake.svg",
+    75: "/icons/snowflake.svg",
+    77: "/icons/snowflake.svg",
+    80: "/icons/cloud-rain-solid.svg",
+    81: "/icons/cloud-showers-heavy.svg",
+    82: "/icons/cloud-bolt.svg",
+    85: "/icons/snowflake.svg",
+    86: "/icons/snowflake.svg",
+    95: "/icons/cloud-bolt.svg"
+  }
+
   const dateFormat = new Intl.DateTimeFormat('en-us', {
     weekday: "long",
     month: "2-digit",
@@ -63,6 +91,7 @@
     <div class="day">
       <h4>{dateFormat.format(date.setDate(date.getDate() + idx))}</h4>
       <p>Weathercode: {weathercode[idx]}</p>
+      <img src="{icons[weathercode[idx]]}" alt="">
       <p>High: {maxTemp[idx]}</p>
       <p>Low: {minTemp[idx]}</p>
       <p>FeelsHigh{feelsLikeMax[idx]}</p>
@@ -82,5 +111,9 @@
   #current{
     display: flex;
     flex-direction: row;
+  }
+  img {
+    height: 32px;
+    width: 32px;
   }
 </style>
