fe9690901169645daa2bf5ab3142a746c1044c7a
max <max>
2023-01-30 07:20:43 -0600

    Taking out some extraneous things

--- src/App.svelte
+++ src/App.svelte
@@ -17,7 +17,7 @@
         .then((data) => {
           currentWeather = data.current_weather;
           dailyWeather = data.daily;
-          console.log(dailyWeather)
+          console.log(typeof dailyWeather)
           feelsLikeMax = [...dailyWeather.apparent_temperature_max];
           time = [...dailyWeather.time]
           loaded = true
@@ -30,25 +30,20 @@
 <main>
   <h1>Weather</h1>
   {#if loaded}
+  <div id="current">
+
     <p>Temp: {currentWeather.temperature}</p>
     <p>Wind: {currentWeather.windspeed}</p>
     <p>Code: {currentWeather.weathercode}</p>
-    <p>Time: {new Date().toLocaleString()}</p>
-    <hr />
-    <p>Apparent Max: {dailyWeather.apparent_temperature_max}</p>
-    <p>Apparent Min: {dailyWeather.apparent_temperature_min}</p>
-    <p>Precip: {dailyWeather.precipitation_sum}</p>
-    <p>Max: {dailyWeather.temperature_2m_max}</p>
-    <p>Max: {dailyWeather.temperature_2m_min}</p>
-    <p>Code: {dailyWeather.weathercode}</p>
-    {#each feelsLikeMax as feelMax}
-      <p>{feelMax}</p>
-    {/each}
-    {#each time as t}
-      <p>{new Date(t)}</p>
-    {/each}
+    <p>Date: {new Date().toDateString()}</p>
+  </div>
+  <hr />
   {/if}
 </main>
 
 <style>
+  #current{
+    display: flex;
+    flex-direction: row;
+  }
 </style>
