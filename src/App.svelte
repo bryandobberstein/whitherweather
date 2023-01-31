a5f162affbb7d1eda1c411ee08af497e2426ff55
max <max>
2023-01-31 07:33:32 -0600

    layout most of the way there

--- src/App.svelte
+++ src/App.svelte
@@ -15,6 +15,7 @@
   let loaded = false;
   const icons = {
     0: "/icons/sunny.svg",
+    1: "/icons/cloud-sun.svg",
     2: "/icons/cloud-sun.svg",
     3: "/icons/cloud.svg",
     45: "/icons/smog.svg",
@@ -80,25 +81,25 @@
 </script>
 
 <main>
-  <h1>Weather</h1>
+  <h1 id="header">Weather</h1>
   {#if loaded}
   <div id="current">
-    <p>Temp: {currentWeather.temperature}</p>
-    <p>Wind: {currentWeather.windspeed}</p>
-    <p>Code: {currentWeather.weathercode}</p>
-    <p>Date: {new Date().toDateString()}</p>
+    <h2 id="currcon">Current</h2>
+    <p id="temp">{currentWeather.temperature}°F</p>
+    <p id="wind">Windspeed: {currentWeather.windspeed}MPH</p>
+    <p id="icon"><img src="{icons[currentWeather.weathercode]}" alt=""></p>
+    <p id="todaysDate">Date: {new Date().toDateString()}</p>
   </div>
-  <hr />
   {#each time as day, idx }
   <div id="daily">
       <h4 id="date">{dateFormat.format(date.setDate(date.getDate() + idx))}</h4>
       <span id="weathercode"><img src="{icons[weathercode[idx]]}" alt=""></span>
-      <span id="high">High: {maxTemp[idx]}</span>
-      <span id="low">Low: {minTemp[idx]}</span>
-      <span id="feelsHigh">FeelsHigh: {feelsLikeMax[idx]}</span>
-      <span id="feelsLow">FeelsLow: {feelsLikeMin[idx]}</span>
-      <span id="rise">Rise: {hourFormat.format(new Date(sunriseTime[idx]))}</span>
-      <span id="set">Set: {hourFormat.format(new Date(sunsetTime[idx]))}</span>
+      <span id="high"><img id="icons-small" src="/icons/temperature-high-solid.svg" alt=""> {maxTemp[idx]}</span>
+      <span id="low"><img id="icons-small" src="/icons/temperature-low-solid.svg" alt=""> {minTemp[idx]}</span>
+      <span id="feelsHigh">Feels like: {feelsLikeMax[idx]}</span>
+      <span id="feelsLow">Feels like: {feelsLikeMin[idx]}</span>
+      <span id="rise"><img id="icons-small" src="/icons/sunny.svg" alt=""> {hourFormat.format(new Date(sunriseTime[idx]))}</span>
+      <span id="set"><img id="icons-small" src="/icons/moon-solid.svg" alt=""> {hourFormat.format(new Date(sunsetTime[idx]))}</span>
       <span id="precip">Precip: {totalPrecip[idx]}</span>
     </div>
     {/each}
@@ -106,15 +107,45 @@
 </main>
 
 <style>
-  #current{
-    display: flex;
-    flex-direction: row;
+  main {
+    display: grid;
+    width: 75vw;
+    justify-content: center;
   }
-  /* img {
-    height: 32px;
-    width: 32px;
-  } */
+  img {
+    height: 64px;
+    width: 64px;
+  }
+  #icons-small{
+    height: 24px;
+  }
+  #header{
+    border-bottom: 3px solid #9c8363;
+  }
+  #current{
+    display: grid;
+    grid-template-areas: ". head ."
+                          "tdate tdate tdate"
+                          "icon temp wind";
+    border-bottom: 3px solid #9c8363;
+  }
+  #currcon{
+    grid-area: head;
+  }
+  #temp {
+    grid-area: temp;
+  }
+  #wind{
+    grid-area: wind;
+  }
+  #icon{
+    grid-area: icon;
+  }
+  #todaysDate{
+    grid-area: tdate;
+  }
   #daily {
+    border-bottom: 3px solid #9c8363;
     display: grid;
     grid-template-areas: "date date date"
                           "code high low"
